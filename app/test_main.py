c88e72e98f5f8136800df9d34a1e86812c2ce173
Artem Yur <artemonodon@gmail.com>
2026-03-14 16:04:08 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,31 +2,31 @@ from pytest_mock import MockerFixture
 from app.main import cryptocurrency_action
 
 
-def test_predicted_exchange_rate_is_low_positive(mocker: MockerFixture) -> None:
+def test_exchange_rate_is_low_positive(mocker: MockerFixture) -> None:
     mocker.patch("app.main.get_exchange_rate_prediction", return_value=105)
 
     assert cryptocurrency_action(100) == "Do nothing"
 
 
-def test_predicted_exchange_rate_is_low_negative(mocker: MockerFixture) -> None:
+def test_exchange_rate_is_low_negative(mocker: MockerFixture) -> None:
     mocker.patch("app.main.get_exchange_rate_prediction", return_value=95)
 
     assert cryptocurrency_action(100) == "Do nothing"
 
 
-def test_predicted_difference_is_not_big(mocker: MockerFixture) -> None:
+def test_difference_is_not_big(mocker: MockerFixture) -> None:
     mocker.patch("app.main.get_exchange_rate_prediction", return_value=101)
 
     assert cryptocurrency_action(100) == "Do nothing"
 
 
-def test_predicted_exchange_rate_is_more(mocker: MockerFixture) -> None:
+def test_exchange_rate_is_more(mocker: MockerFixture) -> None:
     mocker.patch("app.main.get_exchange_rate_prediction", return_value=106)
 
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
-def test_predicted_exchange_rate_is_less(mocker: MockerFixture) -> None:
+def test_exchange_rate_is_less(mocker: MockerFixture) -> None:
     mocker.patch("app.main.get_exchange_rate_prediction", return_value=94)
 
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
